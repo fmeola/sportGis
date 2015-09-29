ec6c5e13d4a7e7dc8afb23462424ae4b2f3c04c4
lbadi <leonelbadi@hotmail.com>
2015-09-29 11:56:03 -0300

    aplicacion para conectarse a twitter

---
+++ twitterPolling/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from twython import Twython
+# Create your views here.
+
+from django.http import HttpResponse
+
+APP_KEY = 'lXx9pK9CsmCtjDcP5t0nrLD82'
+APP_SECRET = 'nNeixWCJ4BdpAPxM0V9XB1IqFqKMjBAxq1TyyK0vhfpgK8NS75'
+
+twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+ACCESS_TOKEN = twitter.obtain_access_token()
+
+def index(request):
+    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+    twits = twitter.search(q='python')
+    print(twits)
+    return render(request, 'index.html', {'twits' : twits})
+    
